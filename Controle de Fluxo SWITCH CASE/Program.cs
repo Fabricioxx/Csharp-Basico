6c9fdcfaed338da579be2df5a0762997af6b7d6d
Fabrício Rosa de França <fabricio476@outlook.com.br>
2022-09-03 23:17:11 -0300

    Controle de fluxo SWITCH CASE e projeto preço peixe

---
+++ Controle de Fluxo SWITCH CASE/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle_de_Fluxo_SWITCH_CASE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            String nome;
+
+            Console.WriteLine("Informe o NOME do peixe : ");
+            nome = Console.ReadLine();
+
+            switch (nome.ToLower()) //ToLower() - Metodo para trasforma toda a entrada de dados em "minusculo"
+            {
+
+                case "tilapia":
+
+                    Console.WriteLine(" {0} - Preço: R$10 Kg", nome);
+                    break;
+
+                case "traira":
+
+                    Console.WriteLine(" {0} - Preço: R$15 Kg", nome);
+                    break;
+
+                case "barigui":
+
+                    Console.WriteLine(" {0} - Preço: R$12 Kg" , nome);
+                    break;
+
+                default:
+
+                    Console.WriteLine("PEIXE Não Cadastrado ");
+
+                    break;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
