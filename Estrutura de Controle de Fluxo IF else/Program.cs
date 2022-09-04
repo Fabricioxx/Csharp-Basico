c97be9b665ab6119a617e211c538ad7358973e19
Fabrício Rosa de França <fabricio476@outlook.com.br>
2022-09-03 22:57:42 -0300

    controle de fluxo if else e projeto aluno aprovado reprovado

---
+++ Estrutura de Controle de Fluxo IF else/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_Controle_de_Fluxo_IF_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Operadores de comparação
+            * 
+            * == igual
+            * != diferente
+            * > maior
+            * < menor
+            * >= maior ou igual
+            * <= menor ou igual
+            * 
+            */
+
+            /* Operadores lógicos
+             * 
+             * && e
+             * || ou
+             * ! não
+             * 
+             */
+
+            //--------------------------
+
+            int notaAluno = 0;
+
+            Console.WriteLine("Informe a Nota do Aluno");
+            notaAluno = (int)Convert.ToSingle(Console.ReadLine()); //paga o resultado da tela converte e quarda na variavel
+
+
+            if(notaAluno < 50 )
+            {
+
+                Console.WriteLine("Aluno REPROVADO !");
+
+            }else if(notaAluno >= 50 && notaAluno < 60)
+            {
+
+                Console.WriteLine("Aluno RECUPERAÇÃO !");
+            }
+            else
+            {
+
+                Console.WriteLine("Aluno APROVADO !");
+            }
+
+
+
+            Console.ReadLine(); //PALSA A TELA
+        }
+    }
+}
